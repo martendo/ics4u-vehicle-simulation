db21e38fb53862e12d0d654276b614c8f5da85a5
martendo <51766598+martendo@users.noreply.github.com>
2024-02-25 23:51:43 -0500

    Improve path creation and drawing
    Divide paths into segments as they are being created to save time during
    rendering, and move functionality into a new class for clarity and ease
    of use.

--- SimulationWorld.java
+++ SimulationWorld.java
@@ -20,10 +20,6 @@ public class SimulationWorld extends World {
 	public static final int WIDTH = 1024;
 	public static final int HEIGHT = 768;
 
-	public static final int PATH_WIDTH = 50;
-	public static final int PATH_OUTLINE_WIDTH = 16;
-	public static final java.awt.Color PATH_COLOR = new java.awt.Color(64, 64, 64);
-
 	private static final int BACKGROUND_PATTERN_WIDTH = 128;
 	private static final java.awt.Color BACKGROUND_PATTERN_COLOR_1 = new java.awt.Color(255, 200, 155);
 	private static final java.awt.Color BACKGROUND_PATTERN_COLOR_2 = new java.awt.Color(255, 190, 140);
@@ -31,13 +27,7 @@ public class SimulationWorld extends World {
 	// Background image drawing facilities
 	private BufferedImage canvas;
 	private Graphics2D graphics;
-	private BasicStroke pathStroke = new BasicStroke(PATH_WIDTH, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND);
-	private BasicStroke pathOutlineStroke = new BasicStroke(PATH_WIDTH + PATH_OUTLINE_WIDTH, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND);
-	private ArrayList<Path2D.Double> paths;
-
-	// Keep track of the last position of the mouse in order to control path curves
-	private int prevMouseX;
-	private int prevMouseY;
+	private ArrayList<SuperPath> paths;
 
 	// Animate the background pattern by shifting it horizontally
 	private int patternShift = 0;
@@ -60,7 +50,7 @@ public class SimulationWorld extends World {
 		graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		graphics.setBackground(new java.awt.Color(0, true));
 
-		paths = new ArrayList<Path2D.Double>();
+		paths = new ArrayList<SuperPath>();
 
 		// Draw initial background image so this world isn't blank on reset
 		updateBackground();
@@ -84,24 +74,19 @@ public class SimulationWorld extends World {
 			return;
 		}
 
+		SuperPath path;
 		if (Greenfoot.mousePressed(null)) {
 			// When mouse changed from non-pressed to pressed state, begin a new path
-			Path2D.Double path = new Path2D.Double();
-			// Set starting point of path to initial mouse position
-			path.moveTo(mouse.getX(), mouse.getY());
+			path = new SuperPath();
 			paths.add(path);
 		} else if (mouse.getButton() != 0 && paths.size() > 0) {
 			// Mouse is currently being dragged -> add a new point to the current path
-			Path2D.Double path = paths.get(paths.size() - 1);
-			// Use quadratic curves to smoothen the lines, connecting midpoints
-			// of mouse positions with actual positions as control points
-			int midx = (mouse.getX() + prevMouseX) / 2;
-			int midy = (mouse.getY() + prevMouseY) / 2;
-			path.quadTo(prevMouseX, prevMouseY, midx, midy);
+			path = paths.get(paths.size() - 1);
+		} else {
+			// Not drawing, nothing to do
+			return;
 		}
-
-		prevMouseX = mouse.getX();
-		prevMouseY = mouse.getY();
+		path.addPoint(mouse.getX(), mouse.getY());
 	}
 
 	/**
@@ -128,88 +113,9 @@ public class SimulationWorld extends World {
 		// Shift the background pattern for the next act
 		patternShift = (patternShift + 1) % (BACKGROUND_PATTERN_WIDTH * 2);
 
-		drawPaths();
-	}
-
-	private void drawPaths() {
-		double[] coords = new double[6];
-		for (Path2D.Double path : paths) {
-			Path2D.Double fill = null;
-			Shape prevSegment = null;
-			double prevx = 0;
-			double prevy = 0;
-			for (PathIterator pi = path.getPathIterator(null); !pi.isDone(); pi.next()) {
-				switch (pi.currentSegment(coords)) {
-				case PathIterator.SEG_MOVETO:
-					prevx = coords[0];
-					prevy = coords[1];
-					Line2D.Double point = new Line2D.Double(prevx, prevy, prevx, prevy);
-
-					// Draw path outline stroke around this path segment
-					graphics.setColor(java.awt.Color.YELLOW);
-					graphics.setStroke(pathOutlineStroke);
-					graphics.draw(point);
-
-					graphics.setColor(PATH_COLOR);
-					graphics.setStroke(pathStroke);
-					graphics.draw(point);
-
-					prevSegment = point;
-					break;
-
-				case PathIterator.SEG_QUADTO:
-					Shape segment;
-					if (Math.hypot(coords[2] - prevx, coords[3] - prevy) < PATH_OUTLINE_WIDTH) {
-						if (fill == null) {
-							fill = new Path2D.Double();
-							fill.moveTo(prevx, prevy);
-						}
-						fill.quadTo(coords[0], coords[1], coords[2], coords[3]);
-						segment = fill;
-					} else {
-						// End the current fill, if any
-						if (fill != null) {
-							prevSegment = fill;
-							fill = null;
-						}
-						// This individual curve is long enough to treat as a segment
-						segment = new QuadCurve2D.Double(prevx, prevy, coords[0], coords[1], coords[2], coords[3]);
-					}
-					// Draw path outline stroke around this path segment
-					graphics.setColor(java.awt.Color.YELLOW);
-					graphics.setStroke(pathOutlineStroke);
-					graphics.draw(segment);
-					// Fill in this path segment
-					graphics.setColor(PATH_COLOR);
-					graphics.setStroke(pathStroke);
-					if (prevSegment != null) {
-						// Draw the preceeding segment over the round cap of this segment's outline
-						// (hide outline showing in between segments)
-						graphics.draw(prevSegment);
-					}
-					graphics.draw(segment);
-
-					if (fill == null) {
-						prevSegment = segment;
-					}
-					prevx = coords[2];
-					prevy = coords[3];
-					break;
-
-				default:
-					break;
-				}
-			}
-			// This path ends in a filled segment
-			if (fill != null) {
-				graphics.setColor(java.awt.Color.YELLOW);
-				graphics.setStroke(pathOutlineStroke);
-				graphics.draw(fill);
-				graphics.setColor(PATH_COLOR);
-				graphics.setStroke(pathStroke);
-				graphics.draw(prevSegment);
-				graphics.draw(fill);
-			}
+		// Draw paths
+		for (SuperPath path : paths) {
+			path.drawUsingGraphics(graphics);
 		}
 	}
 }
---
+++ SuperPath.java
@@ -0,0 +1,129 @@
+import java.awt.Graphics2D;
+import java.awt.BasicStroke;
+import java.awt.Shape;
+import java.awt.geom.Point2D;
+import java.awt.geom.Path2D;
+import java.awt.geom.QuadCurve2D;
+import java.awt.Color;
+import java.util.ArrayList;
+
+/**
+ * A path that is automatically smoothened with quadratic curves between points
+ * and divided into segments to create "filled" portions when points are close
+ * enough together.
+ *
+ * The division of SuperPaths into segments allows them to be drawn with a
+ * stroked outline that follows the path, even when a SuperPath overlaps itself,
+ * where later segments are drawn on top of earlier segments.
+ *
+ * Note: SuperPaths use integer coordinates in order to improve their appearance
+ * when drawn with Graphics2D contexts. (The use of doubles creates artifacts
+ * along paths, particularly at corners, where line joins are sometimes
+ * missing.)
+ *
+ * @author Martin Baldwin
+ */
+public class SuperPath {
+	// Visual parameters
+	public static final int PATH_WIDTH = 50;
+	public static final int PATH_OUTLINE_WIDTH = 16;
+	public static final Color PATH_COLOR = new Color(64, 64, 64);
+	public static final Color PATH_OUTLINE_COLOR = Color.YELLOW;
+
+	// The distance below which to consider points as part of a filled segment
+	public static final double FILL_THRESHOLD = PATH_OUTLINE_WIDTH;
+
+	private static final BasicStroke PATH_STROKE = new BasicStroke(PATH_WIDTH, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND);
+	private static final BasicStroke PATH_OUTLINE_STROKE = new BasicStroke(PATH_WIDTH + PATH_OUTLINE_WIDTH, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND);
+
+	// A single path created from all points added to this SuperPath
+	private Path2D.Double path;
+	// Separated segments for drawing
+	private ArrayList<Shape> segments;
+
+	// Variables to keep track of the last given point in order to control path curves
+	private int prevx;
+	private int prevy;
+
+	/**
+	 * Create a new SuperPath.
+	 */
+	public SuperPath() {
+		path = new Path2D.Double();
+		segments = new ArrayList<Shape>();
+	}
+
+	/**
+	 * Add a new point to this SuperPath.
+	 *
+	 * @param x the x coordinate of the point
+	 * @param y the y coordinate of the point
+	 */
+	public void addPoint(int x, int y) {
+		Point2D prevPoint = path.getCurrentPoint();
+		if (prevPoint == null) {
+			// This is the first point -> begin the path by setting its location
+			path.moveTo(x, y);
+
+			// Add a segment of one point in order to have something to draw
+			Path2D.Double point = new Path2D.Double();
+			point.moveTo(x, y);
+			segments.add(point);
+		} else {
+			// Use quadratic curves to smoothen the lines, connecting midpoints
+			// of given points with actual points as control points
+			int midx = (x + prevx) / 2;
+			int midy = (y + prevy) / 2;
+			path.quadTo(prevx, prevy, midx, midy);
+
+			// Update segments
+			if (prevPoint.distance(midx, midy) < FILL_THRESHOLD) {
+				// This point is close to the previous point, add it to a fill segment
+				Path2D.Double fillSegment;
+				Shape prevSegment = segments.get(segments.size() - 1);
+				if (prevSegment instanceof Path2D.Double) {
+					fillSegment = (Path2D.Double) prevSegment;
+				} else {
+					// Create a new fill segment if the previous segment isn't one
+					fillSegment = new Path2D.Double();
+					fillSegment.moveTo(prevPoint.getX(), prevPoint.getY());
+					segments.add(fillSegment);
+				}
+				fillSegment.quadTo(prevx, prevy, midx, midy);
+			} else {
+				// This individual curve is long enough to treat as a segment on its own
+				QuadCurve2D.Double curve = new QuadCurve2D.Double(prevPoint.getX(), prevPoint.getY(), prevx, prevy, midx, midy);
+				segments.add(curve);
+			}
+		}
+		prevx = x;
+		prevy = y;
+	}
+
+	/**
+	 * Draw this SuperPath using a given graphics context.
+	 *
+	 * @param graphics the Graphics2D context on which to draw this SuperPath.
+	 */
+	public void drawUsingGraphics(Graphics2D graphics) {
+		Shape prevSegment = null;
+		for (Shape segment : segments) {
+			// Draw path outline stroke around this path segment
+			graphics.setColor(PATH_OUTLINE_COLOR);
+			graphics.setStroke(PATH_OUTLINE_STROKE);
+			graphics.draw(segment);
+
+			// Fill in this path segment
+			graphics.setColor(PATH_COLOR);
+			graphics.setStroke(PATH_STROKE);
+			if (prevSegment != null) {
+				// Draw the preceeding segment over the round cap of this segment's outline
+				// (hide outline showing in between segments)
+				graphics.draw(prevSegment);
+			}
+			graphics.draw(segment);
+
+			prevSegment = segment;
+		}
+	}
+}
