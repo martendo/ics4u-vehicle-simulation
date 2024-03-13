d2cbd91de05b0e676f268f1f55ef8921caaac71c
martendo <51766598+martendo@users.noreply.github.com>
2024-03-13 00:51:07 -0400

    Rotate path travellers according to their path

--- Dessert.java
+++ Dessert.java
@@ -1,5 +1,8 @@
 import java.awt.image.BufferedImage;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
 
 /**
  * The "Vehicle" of the simulation.
@@ -9,14 +12,40 @@ import java.awt.Graphics2D;
  */
 public class Dessert extends PathTraveller {
 	private BufferedImage image;
+	private Graphics2D graphics;
+	private Rectangle2D.Double shape;
 
 	public Dessert() {
 		super();
-		image = new BufferedImage(20, 20, BufferedImage.TYPE_INT_ARGB_PRE);
-		Graphics2D graphics = image.createGraphics();
+		image = new BufferedImage(64, 64, BufferedImage.TYPE_INT_ARGB_PRE);
+		graphics = image.createGraphics();
+		shape = new Rectangle2D.Double(-30, -20 / 2, 30, 20);
+		// Turning on antialiasing gives smoother-looking graphics
+		graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+		graphics.setBackground(new java.awt.Color(0, 0, 0, 0));
 		graphics.setColor(java.awt.Color.GREEN);
-		graphics.fillRect(0, 0, 20, 20);
-		graphics.dispose();
+	}
+
+	public void addedToPath(SuperPath path, int laneNum) {
+		super.addedToPath(path, laneNum);
+		// Reflect angle change in image
+		updateImage();
+	}
+
+	public void act() {
+		super.act();
+		// Reflect angle change in image
+		updateImage();
+	}
+
+	private void updateImage() {
+		graphics.clearRect(0, 0, image.getWidth(), image.getHeight());
+		AffineTransform saveAT = graphics.getTransform();
+		// Rotate about and draw the image from the center of this object
+		graphics.translate(image.getWidth() / 2, image.getHeight() / 2);
+		graphics.rotate(angle);
+		graphics.fill(shape);
+		graphics.setTransform(saveAT);
 	}
 
 	/**
--- PathTraveller.java
+++ PathTraveller.java
@@ -1,5 +1,6 @@
 /**
- * An object that travels along a path at a constant speed.
+ * An object that travels along a path at a constant speed, rotating to reflect
+ * its journey along the path.
  *
  * @author Martin Baldwin
  * @version March 2024
@@ -12,6 +13,11 @@ public abstract class PathTraveller extends SuperActor {
 	// The iterator used to progressively travel along this traveller's path
 	private PathTraceIterator pathIter;
 
+	// The exact angle of the current segment of the path
+	private double targetAngle;
+	// The visually pleasing interpolated angle of this traveller, constantly moving towards targetAngle
+	protected double angle;
+
 	/**
 	 * Create a new traveller actor.
 	 */
@@ -19,6 +25,8 @@ public abstract class PathTraveller extends SuperActor {
 		super();
 		path = null;
 		pathIter = null;
+		targetAngle = 0.0;
+		angle = 0.0;
 	}
 
 	/**
@@ -35,6 +43,9 @@ public abstract class PathTraveller extends SuperActor {
 		pathIter.currentSegment(coords);
 		x = coords[0];
 		y = coords[0];
+		// Start with average angle of path's beginning
+		targetAngle = path.getStartAngle();
+		angle = targetAngle;
 	}
 
 	/**
@@ -50,6 +61,17 @@ public abstract class PathTraveller extends SuperActor {
 		// Set this traveller's location to the end point of the iterator's current line segment
 		double[] coords = new double[6];
 		pathIter.currentSegment(coords);
+		// Target angle between previous and new position
+		targetAngle = Math.atan2(coords[1] - y, coords[0] - x);
+		// Interpolate angle towards targetAngle, wrapping around when one direction is closer than the current
+		if (Math.abs(targetAngle - angle) > Math.abs(targetAngle - (angle - Math.PI * 2.0))) {
+			angle -= Math.PI * 2.0;
+		} else if (Math.abs(targetAngle - angle) > Math.abs(targetAngle - (angle + Math.PI * 2.0))) {
+			angle += Math.PI * 2.0;
+		}
+		angle += (targetAngle - angle) * 0.05;
+
+		// Update position
 		x = coords[0];
 		y = coords[1];
 	}
